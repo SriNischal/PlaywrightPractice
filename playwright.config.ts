899529feeebe31ed87681c1a71e69a7ea60f3c44
ATMECS\Srinischal.Thiparani <srinischal.thiparani@atmecs.com>
2023-03-14 17:27:56 +0530

    DemoQA

---
+++ playwright.config.ts
@@ -0,0 +1,53 @@
+import { chromium, PlaywrightTestConfig,defineConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  //retries: process.env.CI ? 2 : 0,
+  retries : 1,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    baseURL : "https://demoqa.com/",
+    viewport : {width : 1366, height : 768},
+    headless : false,
+    screenshot : 'on',
+    video : 'on',
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'retain-on-failure',
+  },
+};
+
+
+export default config;
